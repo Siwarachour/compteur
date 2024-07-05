23e83879450f04a1323e2fe1bdeaeedb4c6aee08
Siwarachour <siwar.achour@esprit.tn>
2024-07-05 13:17:06 +0100

    ordre commit

--- test2.py
+++ test2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import test
 
-
 if 'counters' not in st.session_state:
     st.session_state.counters = [
         {"title": "Counter 1", "initValue": 10},
@@ -11,13 +10,11 @@ if 'counters' not in st.session_state:
         {"title": "Counter 5", "initValue": 50}
     ]
 
-
-if 'sum' not in st.session_state:
-    st.session_state.sum = sum(counter['initValue'] for counter in st.session_state.counters)
-
-
 if 'show_popup' not in st.session_state:
     st.session_state.show_popup = False
+
+def toggle_popup():
+    st.session_state.show_popup = not st.session_state.show_popup
 
 def add_counter(title, init_value):
     new_counter = {
@@ -31,22 +28,14 @@ def add_counter(title, init_value):
 
 def delete_counter(index):
     test.reset(str(index))
-
     st.session_state.counters.pop(index)
-
-
     if f'count_{index}' in st.session_state:
         st.session_state['total_sum'] -= st.session_state[f'count_{index}']
         del st.session_state[f'count_{index}']
-
-    # Reindex counters after deletion
     for i in range(index, len(st.session_state.counters)):
         if f'count_{i+1}' in st.session_state:
             st.session_state[f'count_{i}'] = st.session_state.pop(f'count_{i+1}', 0)
-
-
     update_sum()
-    st.experimental_rerun()
 
 def update_sum():
     st.session_state.sum = test.get_sum()
@@ -55,36 +44,38 @@ def reset_all():
     for i in range(len(st.session_state.counters)):
         test.reset(str(i))
     update_sum()
-    st.experimental_rerun()
 
+if 'sum' not in st.session_state:
+    update_sum()
 
-st.session_state.counters = sorted(st.session_state.counters, key=lambda x: st.session_state.get(f"count_{st.session_state.counters.index(x)}", x['initValue']), reverse=True)
+col1, col2 = st.columns([1, 1])
+with col2:
+    st.button("Add Counter", on_click=toggle_popup)
 
-
-if st.button("Add Counter"):
-    st.session_state.show_popup = True
+with col1:
+    if st.button('Reset'):
+        reset_all()
 
 
 if st.session_state.show_popup:
     with st.form("add_counter_form"):
-        title = st.text_input("Title")
-        init_value = st.number_input("Initial Value", min_value=0, value=0)
-        submitted = st.form_submit_button("Add ")
-        if submitted:
-            add_counter(title, init_value)
+        new_title = st.text_input("Counter Title")
+        new_init_value = st.number_input("Initial Value", value=0, step=1)
+        submit_button = st.form_submit_button("Add")
+        if submit_button:
+            add_counter(new_title, new_init_value)
 
+sorted_indices = sorted(range(len(st.session_state.counters)),
+                        key=lambda i: st.session_state.counters[i]['initValue'],
+                        reverse=True)
 
-if st.button('Reset All'):
-    reset_all()
-
-
-for i, counter in enumerate(st.session_state.counters):
+for i in sorted_indices:
+    counter = st.session_state.counters[i]
     col1, col2 = st.columns([4, 1])
     with col1:
         test.compteur(index=str(i), title=counter["title"], initvalue=int(counter["initValue"]))
     with col2:
         if st.button(f"Delete {counter['title']}", key=f"delete_{i}"):
             delete_counter(i)
-
 
 st.write('Sum:', test.get_sum())
